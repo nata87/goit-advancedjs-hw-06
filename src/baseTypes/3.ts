91316ebd35795e9852f605f3bda83856b6963183
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-02-17 09:43:26 +0100

    fix 1, 3 tasks

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,5 +1,11 @@
-const some: string = 'Text';
+let some: unknown;
+some = 'Text';
 let str: string;
-str = some;
+
+if (typeof some === 'string') {
+    str = some as string;
+} else {
+    str = '';
+}
 
 export {};
